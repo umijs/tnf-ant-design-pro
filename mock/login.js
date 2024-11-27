47880fbabe16d35ca0580551bb185f43fac71828
Wu-kung <1434246346@qq.com>
2024-11-27 19:05:51 +0800

    chore(mock): login

---
+++ mock/login.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'POST /api/login/outLogin': { data: {}, success: true },
+  'POST /api/login/account': {
+    status: 'ok',
+    type: 'account',
+    currentAuthority: 'admin',
+  },
+};
